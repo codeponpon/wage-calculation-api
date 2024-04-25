40b2ca731e58ac46d64dc1461a1daedb444749dd
Pichate Homsang <codeponpon@gmail.com>
2024-04-25 11:15:24 +0700

    feat: setup husky and commitlint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/api/status', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Healthy!!' });
+app.get("/api/status", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Healthy!!" });
 });
 
 const PORT = process.env.PORT || 3000;
